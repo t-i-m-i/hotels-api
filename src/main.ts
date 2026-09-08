5c863bec231cae2546294179f992fb7ccd1cd4b4
timi <timi@timiportfolio.com>
2026-09-08 13:51:42 +0200

    Add static assets for image galleries

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
+import { join } from 'node:path';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { buildOpenApiDocument } from './openapi-document';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(process.cwd(), 'static'), {
+    prefix: '/static/',
+    immutable: true,
+    maxAge: '7d',
+  });
 
   app.enableCors();
   app.enableShutdownHooks();
